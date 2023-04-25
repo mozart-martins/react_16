0c2d9427672675b05b93cd828642070896bf8746
Mozart Dias Martins <mozart.dias.martins@gmail.com>
2023-04-25 15:23:17 -0300

    Finalizando o app 8

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,51 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const randomStringsFromArray = (stringArray, numStrings) => {
+    let resultStringArray = [];
+
+    for (let i = 0; i < numStrings; i++) {
+      let randomString =
+        stringArray[Math.floor(Math.random() * stringArray.length)];
+      resultStringArray.push(randomString);
+    }
+
+    return resultStringArray;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (amount < 1) amount = 1;
+    setText(randomStringsFromArray(data, amount));
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsun?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="text"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
